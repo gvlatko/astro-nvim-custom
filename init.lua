7616c23dabaa8f483c0148922722fc7aacbf8b8b
Vlatko <g.vlatko@gmail.com>
2022-09-09 17:56:50 +0200

    require felina config directly

--- init.lua
+++ init.lua
@@ -342,7 +342,7 @@ local config = {
       },
     },
   },
-  feline = function() require "user.plugins.feline" end,
+  feline = require "user.plugins.feline",
   -- This function is run last and is a good place to configuring
   -- augroups/autocommands and custom filetypes also this just pure lua so
   -- anything that doesn't fit in the normal config locations above can go here
