6705a815138a4954ad4fb0d851e30c2bb6891eeb
Vlatko Gjurgjinoski <g.vlatko@gmail.com>
2022-10-04 15:49:21 +0200

    autoformat on save

--- init.lua
+++ init.lua
@@ -290,15 +290,15 @@ local config = {
       }
       -- set up null-ls's on_attach function
       -- NOTE: You can remove this on attach function to disable format on save
-      -- config.on_attach = function(client)
-      --   if client.resolved_capabilities.document_formatting then
-      --     vim.api.nvim_create_autocmd("BufWritePre", {
-      --       desc = "Auto format before save",
-      --       pattern = "<buffer>",
-      --       callback = vim.lsp.buf.formatting_sync,
-      --     })
-      --   end
-      -- end
+      config.on_attach = function(client)
+        if client.server_capabilities.document_formatting then
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            desc = "Auto format before save",
+            pattern = "<buffer>",
+            callback = vim.lsp.buf.formatting_sync,
+          })
+        end
+      end
       return config -- return final config table to use in require("null-ls").setup(config)
     end,
     treesitter = { -- overrides `require("treesitter").setup(...)`
